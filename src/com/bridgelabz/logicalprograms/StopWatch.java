391abc503fae9c26cf64b41d661305775a57f4da
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-11 20:40:00 +0530

    changed stopwatch

--- src/com/bridgelabz/logicalprograms/StopWatch.java
+++ src/com/bridgelabz/logicalprograms/StopWatch.java
@@ -1,20 +1,25 @@
 package com.bridgelabz.logicalprograms;
 
+import java.time.temporal.Temporal;
 import java.util.Scanner;
+import java.time.Duration;
+import java.time.Instant;
 
 public class StopWatch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter starting time");
-        int h1 = sc.nextInt();
-        int m1 = sc.nextInt();
-        int s1 = sc.nextInt();
-        System.out.println("Enter ending time");
-        int h2 = sc.nextInt();
-        int m2 = sc.nextInt();
-        int s2 = sc.nextInt();
-        System.out.print(h2-h1 +":");
-        System.out.print(m2-m1 +":");
-        System.out.print(s2-s1);
+        System.out.println("Press 0 to start");
+        int startaction= sc.nextInt();
+        if(startaction==0) {
+            Instant start = Instant.now();
+        }
+        System.out.println("Press 1 to end");
+        int endaction= sc.nextInt();
+        else if(endaction==1){
+            Instant end = Instant.now();
+        }
+
+        Duration timeElapsed = Duration.between(start, end);
+        System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
     }
 }
